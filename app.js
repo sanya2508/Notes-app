1a35eebecc21468080b88083d6d83a079fa562ec
sanya2508 <1701sanya@gmail.com>
2021-08-06 05:16:48 +0530

    Done

---
+++ app.js
@@ -0,0 +1,67 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// Add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// List command
+yargs.command({
+    command: 'list',
+    describe: 'List all the notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
---
+++ readme.md
@@ -0,0 +1,21 @@
+## Notes App
+
+### Add a note:
+```
+node app.js add --title="TITLE_NAME" --body="BODY"
+```
+
+### Remove a note:
+```
+node app.js remove --title="TITLE_NAME" 
+```
+
+### List all notes:
+```
+node app.js list
+```
+
+### Read a note:
+```
+node app.js read --title="TITLE_NAME" 
+```
